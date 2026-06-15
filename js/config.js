2215d08e24765286b93e84ea00d424cf21331f20
gsladeira02 <gsladeira02@gmail.com>
2026-06-14 22:32:50 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,4 @@
-// NavalhaOS - Configuração Supabase
-// 1) Crie o projeto no Supabase
-// 2) Rode o arquivo sql/schema.sql
-// 3) Copie a Project URL e a anon public key aqui
-
 window.NAVALHAOS_CONFIG = {
-  SUPABASE_URL: 'https://rxyidpmzuvczevprqiqu.supabase.co/rest/v1/',
+  SUPABASE_URL: 'https://rxyidpmzuvczevprqiqu.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_XGTScRO4yfeKxrEeH7w2Dw_ORLGwxgM'
 };
